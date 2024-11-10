3824ffa720057f3c5f85fe761b61f8245be4002e
tyldaa <wojcicka_kinga@wp.pl>
2024-11-10 18:29:54 +0100

    Add plants list fetching

--- src/pages/PlantListPage.jsx
+++ src/pages/PlantListPage.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { plantsListResponse } from "../api/mocks/plantsListResponse";
+import { apiKey } from "../config";
 
 export const PlantListPage = () => {
-  return <h1>Lista</h1>;
+  const [plants, setPlants] = useState(null);
+
+  useEffect(() => {
+    setPlants(plantsListResponse.data);
+    // fetch(`https://perenual.com/api/species-list?key=${apiKey}&page=1`)
+    //   .then(res => {
+    //     return res.json();
+    //   })
+    //   .then(res => {
+    //     setPlants(res.data);
+    //   })
+    //   .catch(err => {
+    //     // Todo error handling
+    //     console.log(err);
+    //   });
+  }, []);
+  if (!plants) {
+    return <p>Loading plants...</p>;
+  }
+
+  return (
+    <ul>
+      {plants.map(plant => {
+        return <li key={plant.id}>{plant.common_name}</li>;
+      })}
+    </ul>
+  );
 };
